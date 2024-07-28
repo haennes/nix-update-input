c03f73f299a5134be55f35927a32eab23d2d3821
haennes <hannes.hofmuth@gmail.com>
2024-07-28 23:06:38 +0200

    fmt

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
   description = "update-input";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
 
-  outputs = {self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       forAllSystems = function:
         nixpkgs.lib.genAttrs [
@@ -13,60 +11,58 @@
           "aarch64-linux"
           "x86_64-darwin"
           "aarch64-darwin"
-        ]
-          (system: function nixpkgs.legacyPackages.${system});
+        ] (system: function nixpkgs.legacyPackages.${system});
       script = pkgs: ''
-          set -euo pipefail
-          input=$(                                           \
-            nix flake metadata --json                        \
-            | ${pkgs.jq}/bin/jq -r ".locks.nodes.root.inputs | keys[]" \
-            | printf "$(</dev/stdin) \nall" \
-            | ${pkgs.fzf}/bin/fzf)
-          if [[ $input == "all" ]];
-          then
-            nix flake update
-          else
-            nix flake lock --update-input $input
-          fi
-        '';
-      _script_apply =
-      ''
-          while true; do
-              read -p "apply? " yn
-              case $yn in
-                  [Yy]* ) sudo nixos-rebuild switch --flake .#; break;;
-                  [Nn]* ) exit;;
-                  * ) echo "Please answer yes or no.";;
-              esac
-          done
+        set -euo pipefail
+        input=$(                                           \
+          nix flake metadata --json                        \
+          | ${pkgs.jq}/bin/jq -r ".locks.nodes.root.inputs | keys[]" \
+          | printf "$(</dev/stdin) \nall" \
+          | ${pkgs.fzf}/bin/fzf)
+        if [[ $input == "all" ]];
+        then
+          nix flake update
+        else
+          nix flake lock --update-input $input
+        fi
       '';
-      script_apply = pkgs: (nixpkgs.lib.concatLines [
-      (script pkgs)
-      _script_apply
-      ]);
-      script_apply_cycle = pkgs: (nixpkgs.lib.concatLines [
-      ''while true; do''
-        (script pkgs)
-        _script_apply
-      ''
-        read -p "continue?" cont
-        case $cont in
-          [Yy]* ) ;;
-          [Nn]* ) exit;;
-          * ) echo "Please answer yes or no.";;
-        esac
-      done
-      ''
-      ]);
+      _script_apply = ''
+        while true; do
+            read -p "apply? " yn
+            case $yn in
+                [Yy]* ) sudo nixos-rebuild switch --flake .#; break;;
+                [Nn]* ) exit;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+      '';
+      script_apply = pkgs:
+        (nixpkgs.lib.concatLines [ (script pkgs) _script_apply ]);
+      script_apply_cycle = pkgs:
+        (nixpkgs.lib.concatLines [
+          "while true; do"
+          (script pkgs)
+          _script_apply
+          ''
+              read -p "continue?" cont
+              case $cont in
+                [Yy]* ) ;;
+                [Nn]* ) exit;;
+                * ) echo "Please answer yes or no.";;
+              esac
+            done
+          ''
+        ]);
     in {
-      overlays.default = final: prev:
-      {
-      inherit (self.packages.x86_64-linux) update-apply-cycle;
+      overlays.default = final: prev: {
+        inherit (self.packages.x86_64-linux) update-apply-cycle;
       };
       packages = forAllSystems (pkgs: {
         default = pkgs.writeShellScriptBin "update-input" (script pkgs);
-        update-apply = pkgs.writeShellScriptBin "update-apply" (script_apply pkgs);
-        update-apply-cycle = pkgs.writeShellScriptBin "update-apply-cycle" (script_apply_cycle pkgs);
+        update-apply =
+          pkgs.writeShellScriptBin "update-apply" (script_apply pkgs);
+        update-apply-cycle = pkgs.writeShellScriptBin "update-apply-cycle"
+          (script_apply_cycle pkgs);
       });
     };
 }
